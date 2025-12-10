e3625080803c1fecf7438ecfc34420e5eb6ba660
dlai-sd <yogeshk7377@gmail.com>
2025-12-10 17:37:26 +0000

    add: test directory structure with placeholder widget test

---
+++ mobile/test/widget_test.dart
@@ -0,0 +1,11 @@
+// This file is a placeholder for future widget tests
+// Tests will be added during development
+
+void main() {
+  // TODO: Add widget tests here
+  // Example:
+  // testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  //   await tester.pumpWidget(const MyApp());
+  //   expect(find.text('0'), findsOneWidget);
+  // });
+}
