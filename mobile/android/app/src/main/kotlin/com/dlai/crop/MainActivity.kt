38ea06c23342e7aebd55ae0022e89e30df1c1098
dlai-sd <yogeshk7377@gmail.com>
2025-12-11 16:41:34 +0000

    fix: remove incompatible android.window.SplashScreen import (requires API 31, minSdk is 21)

--- mobile/android/app/src/main/kotlin/com/dlai/crop/MainActivity.kt
+++ mobile/android/app/src/main/kotlin/com/dlai/crop/MainActivity.kt
@@ -2,7 +2,6 @@ package com.dlai.crop
 
 import android.os.Build
 import android.os.Bundle
-import android.window.SplashScreen.OnExitAnimationListener
 import androidx.core.view.WindowCompat
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
